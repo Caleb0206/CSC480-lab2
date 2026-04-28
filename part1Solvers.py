5d599156e137fba3beac28e47645a6fe8b3da2f0
calebhuang <calebhuang0206@gmail.com>
2026-04-28 13:32:24 -0700

    part1 - Proof by unsat, Demorgans, Wedding planning, sudoku

--- part1Solvers.py
+++ part1Solvers.py
@@ -1,5 +1,5 @@
 import z3
-from z3 import (Solver, Bool,Bools,Int,Ints, Or, Not, And, Implies, Xor, BitVec, BV2Int, Reals, Distinct, If)
+from z3 import (Solver, Bool, Bools, Int, Ints, Or, Not, And, Implies, Xor, BitVec, BV2Int, Reals, Distinct, If)
 
 """
 Suppose we want to find a satisfying assignment for the expression (a || !b) && (!a || c). That means we’re looking for values of a, b, and c that make the entire expression evaluate to true.
@@ -8,8 +8,12 @@ One way to approach this is by checking all possible combinations of truth value
 
 Can you list all 8 combinations of a, b, and c?
 
-In general, for n variables, there will be 2^n possible assignments, so the approach of "trying everything" is not efficient for large n. In fact, this is the SAT ("satisfiability") problem, which is NP-complete, meaning that it is "one of the hardest problems to which solutions can be verified quickly. But this difficult problem is exactly what tools like the Z3 SMT solver! Here’s how we can solve the problem using the Z3:
+In general, for n variables, there will be 2^n possible assignments, so the approach of "trying everything" is not efficient for large n. In fact, this is the SAT ("satisfiability") problem, 
+which is NP-complete, meaning that it is "one of the hardest problems to which solutions can be verified quickly. 
+But this difficult problem is exactly what tools like the Z3 SMT solver! Here’s how we can solve the problem using the Z3:
 """
+
+
 def boolean_expressions():
     a = Bool('a')
     b = Bool('b')
@@ -17,8 +21,8 @@ def boolean_expressions():
 
     s = Solver()
 
-    clause_1 = Or(a,Not(b))
-    clause_2 = Or(Not(a),c)
+    clause_1 = Or(a, Not(b))
+    clause_2 = Or(Not(a), c)
 
     s.add(clause_1)
     s.add(clause_2)
@@ -36,8 +40,7 @@ def boolean_expressions():
     In this case, we can add an additional constraint that says that we don’t want the solution where a, b, and c are all false.
     """
 
-
-    clause_3 = Not(And(Not(a),Not(b),Not(c)))
+    clause_3 = Not(And(Not(a), Not(b), Not(c)))
     s.add(clause_3)
 
     match s.check():
@@ -51,7 +54,7 @@ def boolean_expressions():
     Let’s try adding one more constraint
     """
 
-    clause_4 = (And(Not(a),b))
+    clause_4 = (And(Not(a), b))
     s.add(clause_4)
     match s.check():
         case z3.unsat:
@@ -61,97 +64,123 @@ def boolean_expressions():
     """
 
 
-
-
-
 """
 Z3 can find solutions to more than just SAT problems – it is an SMT solver.
 
-SMT stands for SAT modulo theories; it generalizes SAT to formulas involving integers, strings, arrays, and so on (the details of how it can do this are interesting but complicated, and we probably wont be able to get to them in class). For example, we can use Z3 to find a solution for the system of equations 3x - 2y = 1, y - x = 1.
+SMT stands for SAT modulo theories; it generalizes SAT to formulas involving integers, strings, arrays, and so on 
+(the details of how it can do this are interesting but complicated, and we probably wont be able to get to them in class). For example, we can use Z3 to find a solution for the system of equations 3x - 2y = 1, y - x = 1.
 """
+
+
 def integer_expressions():
-    x,y = Ints('x y')
-    s=Solver()
-    s.add(3*x-2*y==1)
-    s.add(y-x==1)
+    x, y = Ints('x y')
+    s = Solver()
+    s.add(3 * x - 2 * y == 1)
+    s.add(y - x == 1)
 
     match s.check():
         case z3.sat:
             model = s.model()
             print(model)
 
+
 """
-In addition to integers Z3 can solve systems of equations that use real numbers. These can get quite complex and also very useful potentially for some of your final projects.
+In addition to integers Z3 can solve systems of equations that use real numbers. 
+These can get quite complex and also very useful potentially for some of your final projects.
 """
+
 
 def real_artithmetic():
     x, y = Reals('x y')
-    s=Solver()
+    s = Solver()
     s.add(x != 0)
     s.add(y != 0)
     s.add(x + y == 4 * x * y)
     match s.check():
         case z3.sat:
-            print (f"Model: {s.model()}")
+            print(f"Model: {s.model()}")
 
         case z3.unsat:
-            print ("UNSAT")
+            print("UNSAT")
+
 
 """
-One of the most prominent uses of SMT in industry (especially now when so much software is being produced with little else to ensure correctness) is in software verification. You can see how we can prove interesting properties about programs including relating logical properties with underlying representation properties.
+One of the most prominent uses of SMT in industry (especially now when so much software is being produced with little
+ else to ensure correctness) is in software verification. You can see how we can prove interesting properties about
+programs including relating logical properties with underlying representation properties.
 """
+
+
 def integer_overflow():
     # Note this takes a long time to process!
     x = BitVec('x', 16)
     y = BitVec('y', 16)
 
-    s=Solver()
-    s.add((BV2Int(x) + BV2Int(y)) != BV2Int(x+y))
+    s = Solver()
+    s.add((BV2Int(x) + BV2Int(y)) != BV2Int(x + y))
     match s.check():
         case z3.sat:
-            print (f"Model: {s.model()}")
+            print(f"Model: {s.model()}")
 
         case z3.unsat:
-            print ("UNSAT")
-
-
+            print("UNSAT")
 
 
 """
-Often, SMT solvers are not used to find a solution, but to prove that no solution exists. For instance, say that we want to prove the mathematical fact y > 0 ==> x + y > x. How might we do this using z3?
+Often, SMT solvers are not used to find a solution, but to prove that no solution exists. For instance, say that we want 
+to prove the mathematical fact y > 0 ==> x + y > x. How might we do this using z3?
 """
+
+
 def proof_by_unsat():
     # y>0 => x+y > x
-    x,y = Ints('x y')
+    x, y = Ints('x y')
     s = Solver()
 
     # TODO: YOUR CODE HERE
+    s.add(y > 0)
+    s.add(x + y <= x)
 
     match s.check():
         case z3.unsat:
-           print("The formula is UNSAT, meaning no contradiction can be found. Therefore the original formula must always be true.  QED.")
+            print(
+                "The formula is UNSAT, meaning no contradiction can be found. Therefore the original formula must always be true.  QED.")
 
 
 """
 Similarly, how might we try to generally prove any valid logical formula such as De Morgan's Laws?
 """
+
+
 def demorgans_proof():
     p, q = Bools('p q')
     demorgan = And(p, q) == Not(Or(Not(p), Not(q)))
 
     def prove(f):
         """
-        Print "No counterexample can be found, therefore the statement is true" if the given formula f is true, otherwise print "The formula f is false, with counterexample given by: " and the model that shows the formula to be false.
+        Print "No counterexample can be found, therefore the statement is true" if the given formula f is true,
+        otherwise print "The formula f is false, with counterexample given by: " and the model that shows the formula to be false.
         """
         # TODO: YOUR CODE HERE
-        pass
+        s = Solver()
+        # see if counterexample is true
+        s.add(Not(f))
+
+        match s.check():
+            case z3.unsat:
+                print("No conterexample can be found, therefore the statement is true:", f)
+            case z3.sat:
+                print("The formula f is false, with counterexample given by: ", s.model())
 
     prove(demorgan)
 
 
 """
-Let us try to use z3 to solve a classic kind of puzzle. You will need to come up with an encoding of the relevant part of the puzzle into variables and constraints as well translate the output of the solver into a solution.
+Let us try to use z3 to solve a classic kind of puzzle. You will need to come up with an encoding of the 
+relevant part of the puzzle into variables and constraints as well translate the output of the solver into a solution.
 """
+
+
 def wedding_planning():
     """
     You need to assign seating for a table of three potentially contentious wedding guests.
@@ -175,51 +204,105 @@ def wedding_planning():
     or
         "There is no acceptable seating arraignment"
     """
-    #TODO: YOUR CODE HERE
+    # TODO: YOUR CODE HERE
+    alice, bob, charlie = Ints('alice bob charlie')
+    s = Solver()
 
+    # between left middle and right
+    s.add(alice >= 0, alice <= 2)
+    s.add(bob >= 0, bob <= 2)
+    s.add(charlie >= 0, charlie <= 2)
 
+    # can't equal one another
+    s.add(And(alice != bob, bob != charlie, alice != charlie))
 
+    # alice cannot sit next to charlie
+    s.add(And(alice + 1 != charlie, alice - 1 != charlie))
+    # alice cannot sit in the leftmost chair
+    s.add(alice != 0)
+
+    # bob cannot sit to the right of charlie
+    s.add(bob < charlie)
+
+    match s.check():
+        case z3.sat:
+            print(f"Alice sits on the left, Charlie in the middle, and Bob on the right. {s.model()}")
+        case z3.unsat:
+            print("There is no acceptable seating arrangement")
 
 
 """
 Lets try to solve a more complex puzzle like sudoku, given an arbitrary input.
 
-In case you do not know, sudoku is a puzzle where the goal is to insert numbers in boxes to satisfy the following condition: each row, column, and 3x3 box must contain the digits 1 through 9 exactly once.
+In case you do not know, sudoku is a puzzle where the goal is to insert numbers in boxes to satisfy the following condition: 
+each row, column, and 3x3 box must contain the digits 1 through 9 exactly once.
 
-You will need to transform an arbitrary sudoku puzzle template (where zero represents the unfilled boxes) into a solved puzzle (or show that the puzzle is impossible).
+You will need to transform an arbitrary sudoku puzzle template (where zero represents the unfilled boxes) into a solved
+ puzzle (or show that the puzzle is impossible).
 """
 
+
 def print_sudoku(grid):
-    for i,row in enumerate(grid):
-        if i%3==0:
-            print('-'*25)
+    for i, row in enumerate(grid):
+        if i % 3 == 0:
+            print('-' * 25)
         print(f'| {row[0]} {row[1]} {row[2]} | {row[3]} {row[4]} {row[5]} | {row[6]} {row[7]} {row[8]} |')
-    print('-'*25)
+    print('-' * 25)
 
 
 def sudoku(puzzle):
-
     """
     Use print_sudoku to print your solution to puzzle or otherwise print "The puzzle is impossible.".
     """
-    #TODO: YOUR CODE HERE
+    # TODO: YOUR CODE HERE
+    grid = [[Int(f'cell{r}_{c}') for c in range(9)] for r in range(9)]
+
+    s = Solver()
+    # Distinct Rows
+    for row in grid:
+        for cell in row:
+            s.add(And(cell >= 1, cell <= 9))
+
+        s.add(Distinct(row))
+
+    # Distinct columns
+    for c in range(9):
+        s.add(Distinct(grid[0][c], grid[1][c], grid[2][c], grid[3][c], grid[4][c],
+                       grid[5][c], grid[6][c], grid[7][c], grid[8][c]))
+
+    # Distinct boxes
+    for r in range(0, 9, 3):
+        for c in range(0, 9, 3):
+            s.add(Distinct(grid[r][c], grid[r][c + 1], grid[r][c + 2],
+                           grid[r + 1][c], grid[r + 1][c + 1], grid[r + 1][c + 2],
+                           grid[r + 2][c], grid[r + 2][c + 1], grid[r + 2][c + 2]))
+    for r in range(9):
+        for c in range(9):
+            if puzzle[r][c] != 0:
+                s.add(grid[r][c] == puzzle[r][c])
+    match s.check():
+        case z3.sat:
+            m = s.model()
+            solved_grid = [[m[grid[r][c]].as_long() for c in range(9)] for r in range(9)]
+            print_sudoku(solved_grid)
+        case z3.unsat:
+            print("The puzzle is impossible.")
 
 
-
-instance = ((0,0,0,0,9,4,0,3,0),
-            (0,0,0,5,1,0,0,0,7),
-            (0,8,9,0,0,0,0,4,0),
-            (0,0,0,0,0,0,2,0,8),
-            (0,6,0,2,0,1,0,5,0),
-            (1,0,2,0,0,0,0,0,0),
-            (0,7,0,0,0,0,5,2,0),
-            (9,0,0,0,6,5,0,0,0),
-            (0,4,0,9,7,0,0,0,0))
-
-
+instance = ((0, 0, 0, 0, 9, 4, 0, 3, 0),
+            (0, 0, 0, 5, 1, 0, 0, 0, 7),
+            (0, 8, 9, 0, 0, 0, 0, 4, 0),
+            (0, 0, 0, 0, 0, 0, 2, 0, 8),
+            (0, 6, 0, 2, 0, 1, 0, 5, 0),
+            (1, 0, 2, 0, 0, 0, 0, 0, 0),
+            (0, 7, 0, 0, 0, 0, 5, 2, 0),
+            (9, 0, 0, 0, 6, 5, 0, 0, 0),
+            (0, 4, 0, 9, 7, 0, 0, 0, 0))
 
 """
-The final puzzle you will solve using Z3 is the Coin Sum problem. Here in the US we have (or used to have) pennies worth 1 cent, nickels worth 5 cents, dimes worth 10 cents, quarters worth 25 cents, half dollar coins worth 50 cents, and dollar coins worth 100 cents.
+The final puzzle you will solve using Z3 is the Coin Sum problem. Here in the US we have (or used to have) pennies 
+worth 1 cent, nickels worth 5 cents, dimes worth 10 cents, quarters worth 25 cents, half dollar coins worth 50 cents,
+and dollar coins worth 100 cents.
 
 It is possible to make $2 using these coins in the following way:
 
@@ -228,9 +311,10 @@ It is possible to make $2 using these coins in the following way:
 The question is: How many ways can $2 be made using any number of coins?
 """
 
+
 def coin_sum(total):
     # Variables for the numbers of each coin denomination
-    p,n,d,q,f,d = Ints('p n d q f d')
+    p, n, d, q, f, d = Ints('p n d q f d')
 
     """
     Print the number of ways the $2 can be made using any number of the above coins.
