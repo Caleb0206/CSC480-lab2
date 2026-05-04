7c95263dd76eb1f8200e14a91dfb33a01d61268a
calebhuang <calebhuang0206@gmail.com>
2026-05-03 17:53:12 -0700

    added rank var to track 1 single loop, ensure not a Wall, Imply rules for rank, s.check, sat case builds one planned moves set, unsat case

--- part2Agents.py
+++ part2Agents.py
@@ -6,7 +6,7 @@ from model import (
     WizardMoves,
     GameAction,
     GameState,
-    WizardSpells, NeutralStone,
+    WizardSpells, NeutralStone, Wall,
 )
 from agents import WizardAgent
 
@@ -28,44 +28,89 @@ class PuzzleWizard(WizardAgent):
     def react(self, state: GameState) -> WizardMoves:
         fire_stones = state.get_all_tile_locations(FireStone)
         ice_stones = state.get_all_tile_locations(IceStone)
-        grid_size = state.grid_size
+        row_size, col_size = state.grid_size
         wizard_location = state.active_entity_location
 
         # TODO: YOUR CODE HERE
+        # add wall_locations -> unusable
+        wall_locations = state.get_all_tile_locations(Wall)
+
+        # Add a planned moves storage to solve once instead of every time
+        if hasattr(self, "planned") and self.planned:
+            return self.planned.pop(0)
+
+        # grid - up/down/left/right = Boolean values, rank = order for only 1 cycle/loop
         grid = [
             [{"up": Bool(f"{r}_{c}_up"),
               "down": Bool(f"{r}_{c}_down"),
               "left": Bool(f"{r}_{c}_left"),
-              "right": Bool(f"{r}_{c}_right")} for c in range(grid_size)]
-            for r in range(grid_size)
+              "right": Bool(f"{r}_{c}_right"),
+              "rank": Int(f"{r}_{c}_rank")} for c in range(col_size)]
+            for r in range(row_size)
         ]
-
+        # initialize Solver
         s = Solver()
-        for r in range(grid_size):
-            for c in range(grid_size):
+        for r in range(row_size):
+            for c in range(col_size):
                 cell = grid[r][c]
-
+                # if cell is on the border of the grid, make edge booleans False
                 if r == 0:
                     s.add(cell["up"] == False)
-                if r == grid_size - 1:
+                if r == row_size - 1:
                     s.add(cell["down"] == False)
                 if c == 0:
                     s.add(cell["left"] == False)
-                if c == grid_size - 1:
+                if c == col_size - 1:
                     s.add(cell["right"] == False)
 
-                if c + 1 < grid_size:
+                # cell's direction boolean should equal neighbor's opposite corresponding direction boolean
+                if c + 1 < col_size:
                     s.add(cell["right"] == grid[r][c + 1]["left"])
                 if c - 1 >= 0:
                     s.add(cell["left"] == grid[r][c - 1]["right"])
-                if r + 1 < grid_size:
+                if r + 1 < row_size:
                     s.add(cell["down"] == grid[r + 1][c]["up"])
                 if r - 1 >= 0:
                     s.add(cell["up"] == grid[r - 1][c]["down"])
 
-                # ensure all cells need to be unvisited or gone through once (deg == 2)
-                deg = If(cell["left"], 1, 0) + If(cell["right"], 1, 0) + If(cell["down"], 1, 0) + If(cell["up"], 1, 0)
-                s.add(Or(deg == 0, deg == 2))
+                # Calculate degree: all cells need to be unvisited or gone through once (deg == 2)
+                deg = (If(cell["left"], 1, 0) +
+                       If(cell["right"], 1, 0) +
+                       If(cell["down"], 1, 0) +
+                       If(cell["up"], 1, 0))
+
+                # Ensure the cell is not a Wall tile
+                if Location(r, c) in wall_locations:
+                    s.add(deg == 0)
+                else:
+                    # Otherwise, degree is 0 (unused), or 2 (passes through)
+                    s.add(Or(deg == 0, deg == 2))
+
+                # ensure all cells ranks
+                if r == wizard_location.row and c == wizard_location.col:
+                    # Starting Cell = Wizard Location, must connect back
+                    s.add(deg == 2)
+                    s.add(cell["rank"] == 0)  # rank = 0 because needs to end at Wizard
+                else:
+                    # Non-Wizard, unused rank == -1, used rank > 0
+                    s.add(Implies(deg == 0, cell["rank"] == -1))
+                    s.add(Implies(deg == 2, cell["rank"] > 0))
+
+                    # if non-wizard is used, at least one neighbor has smaller rank
+                    small_neighbors = []
+
+                    # check for in-bound smaller neighbors
+                    if c + 1 < col_size:
+                        small_neighbors.append(And(cell["right"], grid[r][c + 1]["rank"] < cell["rank"]))
+                    if c - 1 >= 0:
+                        small_neighbors.append(And(cell["left"], grid[r][c - 1]["rank"] < cell["rank"]))
+                    if r - 1 >= 0:
+                        small_neighbors.append(And(cell["up"], grid[r - 1][c]["rank"] < cell["rank"]))
+                    if r + 1 < row_size:
+                        small_neighbors.append(And(cell["down"], grid[r + 1][c]["rank"] < cell["rank"]))
+
+                    # add rule that if cell was passed through, its neighbors should be smaller ranked
+                    s.add(Implies(deg == 2, Or(small_neighbors)))
 
         # Stones
         for fs in fire_stones:
@@ -79,7 +124,7 @@ class PuzzleWizard(WizardAgent):
             if fs.row - 1 >= 0:
                 up = grid[fs.row - 1][fs.col]
 
-                if fs.col + 1 < grid_size:
+                if fs.col + 1 < col_size:
                     right = grid[fs.row][fs.col + 1]
                     s.add(Implies(And(cell["up"], cell["right"]),
                                   And(self.straight(up), self.straight(right))))
@@ -87,10 +132,10 @@ class PuzzleWizard(WizardAgent):
                     left = grid[fs.row][fs.col - 1]
                     s.add(Implies(And(cell["up"], cell["left"]),
                                   And(self.straight(up), self.straight(left))))
-            if fs.row + 1 < grid_size:
+            if fs.row + 1 < row_size:
                 down = grid[fs.row + 1][fs.col]
 
-                if fs.col + 1 < grid_size:
+                if fs.col + 1 < col_size:
                     right = grid[fs.row][fs.col + 1]
                     s.add(Implies(And(cell["down"], cell["right"]),
                                   And(self.straight(down), self.straight(right))))
@@ -106,21 +151,65 @@ class PuzzleWizard(WizardAgent):
             # needs to be a straight
             s.add(self.straight(cell))
 
-            # Either two neighboring cells must be a turn
-            if ice.row - 1 >= 0 and ice.row + 1 < grid_size:
+            # At least 1 neighboring cell must be a turn
+            if ice.row - 1 >= 0 and ice.row + 1 < row_size:
                 up = grid[ice.row - 1][ice.col]
                 down = grid[ice.row + 1][ice.col]
 
                 s.add(Implies(And(cell["up"], cell["down"], Not(cell["left"]), Not(cell["right"])),
                               Or(self.turn(up), self.turn(down))))
 
-            if ice.col - 1 >= 0 and ice.col + 1 < grid_size:
+            if ice.col - 1 >= 0 and ice.col + 1 < col_size:
                 right = grid[ice.row][ice.col + 1]
                 left = grid[ice.row][ice.col - 1]
                 s.add(Implies(And(cell["left"], cell["right"], Not(cell["up"]), Not(cell["down"])),
                               Or(self.turn(left), self.turn(right))))
 
-        return MASYU_1_SOLUTION.pop(0)
+        match s.check():
+            case z3.sat:
+                m = s.model()
+
+                start = wizard_location
+                current_loc = start
+                previous = None
+                moves = []
+
+                while True:
+                    cell = grid[current_loc.row][current_loc.col]
+
+                    # check the valid moves
+                    valid = []
+                    if z3.is_true(m[cell["up"]]):
+                        valid.append((WizardMoves.UP, Location(current_loc.row - 1, current_loc.col)))
+                    if z3.is_true(m[cell["down"]]):
+                        valid.append((WizardMoves.DOWN, Location(current_loc.row + 1, current_loc.col)))
+                    if z3.is_true(m[cell["left"]]):
+                        valid.append((WizardMoves.LEFT, Location(current_loc.row, current_loc.col - 1)))
+                    if z3.is_true(m[cell["right"]]):
+                        valid.append((WizardMoves.RIGHT, Location(current_loc.row, current_loc.col + 1)))
+
+                    chosen_move = None
+                    chosen_next = None
+                    for dir, next_loc in valid:
+                        # do not return an already visited location
+                        if next_loc != previous:
+                            chosen_move = dir
+                            chosen_next = next_loc
+                            break
+                    moves.append(chosen_move)
+
+                    previous = current_loc
+                    current_loc = chosen_next
+
+                    if current_loc == start:
+                        break;
+                self.planned = moves
+                return self.planned.pop(0)
+
+            case z3.unsat:
+                raise RuntimeError("Could not find a path for the Masyu map.")
+
+        # return MASYU_1_SOLUTION.pop(0)
 
 
 class SpellCastingPuzzleWizard(WizardAgent):
